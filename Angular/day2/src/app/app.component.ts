9b071904addb592e292c30da00d25a5e7c2219e2
Manar Khaled <94627280+manar255@users.noreply.github.com>
2025-02-04 16:13:48 +0200

    Add footer, header, and product components with styles; implement category and product models

--- Angular/day2/src/app/app.component.ts
+++ Angular/day2/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../Components/header/header.component';
+import { ProductComponent } from '../Components/product/product.component';
+import { FootorComponent } from '../Components/footor/footor.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,ProductComponent,FootorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
