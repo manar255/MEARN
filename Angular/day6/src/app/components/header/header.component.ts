5754eaa55a9f8a54bd05199d3efff1cf8345012c
manar255 <manarkhaled2510@gmail.com>
2025-02-15 19:24:36 +0200

    Enhance login and register components with new styles and layout; add Font Awesome integration

--- Angular/day6/src/app/components/header/header.component.ts
+++ Angular/day6/src/app/components/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  isBasic:boolean=false;
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isBasic = (event.url == '/login' || event.url == '/register');
+      }
+    });
+  }
 }
